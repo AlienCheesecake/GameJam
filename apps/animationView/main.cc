ad3c7cec24ec0676860da14489ade75ce2553482
Alien Cheesecake <AlienCheesecake@yandex.ru>
2024-06-13 15:56:50 +0300

    Я сделала самое крутое меню из всех меню !!!!

--- apps/animationView/main.cc
+++ apps/animationView/main.cc
@@ -8,47 +8,174 @@
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <nlohmann/json.hpp>
+#include "scdc/scene_compose.hh"
+#include "mmedia/AssetManager.hh"
+#include "mmedia/MusicPlayer.hh"
+
+using namespace scdc;
+using namespace mmed;
+
+struct Mehehenu:Scene
+{
+  sf::RenderWindow &win;
+
+  sf::Texture background1 = AssetManager::getTexture("images/fon1.png");
+  sf::Texture background2 = AssetManager::getTexture("images/fon2.png");
+  sf::Sprite bg;
+
+  sf::Sprite button_start;
+  sf::Texture start = AssetManager::getTexture("images/start.png");
+  sf::Texture start_select = AssetManager::getTexture("images/start_select.png");
+  sf::Texture start_final = AssetManager::getTexture("images/start_final.png");
+
+  sf::Sprite button_exit;
+  sf::Texture exit = AssetManager::getTexture("images/exit.png");
+  sf::Texture exit_select = AssetManager::getTexture("images/exit_select.png");
+  sf::Texture exit_final = AssetManager::getTexture("images/exit_final.png");
+
+  MusicField mf{"audio/goofy.ogg"};
+  Mehehenu(SceneCompose &cmp, sf::RenderWindow &win) : Scene(cmp), win(win)
+  {
+    button_start.setPosition(750, 200);
+    button_exit.setPosition(790, 400);
+    bg.setTexture(background2);
+    button_start.setTexture(start);
+    button_exit.setTexture(exit);
+  }
+  void draw(sf::RenderTarget &window) override 
+  { 
+    ::draw(window, bg); 
+    ::draw(window, button_start);
+    ::draw(window, button_exit);
+  }
+  bool update(sf::Time dt) override 
+  {
+    //ca_.update(dt);
+    return false;
+  }
+  bool handleEvent(const sf::Event &event) override 
+  { 
+    sf::Vector2i pos = sf::Mouse::getPosition(win);
+    sf::Vector2f mousepos = win.mapPixelToCoords(pos);
+    if(button_start.getGlobalBounds().contains(mousepos.x, mousepos.y))
+    {
+      button_start.setTexture(start_select);
+      if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+      {
+        button_start.setTexture(start_final);
+      }
+    }
+    else if(button_exit.getGlobalBounds().contains(mousepos.x, mousepos.y))
+    {
+      button_exit.setTexture(exit_select);
+      if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+      {
+        button_exit.setTexture(exit_final);
+        cmp_.pending_pop();
+      }
+    }
+    else
+    {
+      button_start.setTexture(start);
+      button_exit.setTexture(exit);
+    }
+    return true; 
+  }
+};
+
+struct Menu:Scene
+{
+  sf::RenderWindow &win;
+
+  sf::Texture background1 = AssetManager::getTexture("images/fon1.png");
+  sf::Texture background2 = AssetManager::getTexture("images/fon2.png");
+  sf::Sprite bg;
+
+  sf::Sprite button_start;
+  sf::Texture start = AssetManager::getTexture("images/start.png");
+  sf::Texture start_select = AssetManager::getTexture("images/start_select.png");
+  sf::Texture start_final = AssetManager::getTexture("images/start_final.png");
+
+  sf::Sprite button_exit;
+  sf::Texture exit = AssetManager::getTexture("images/exit.png");
+  sf::Texture exit_select = AssetManager::getTexture("images/exit_select.png");
+  sf::Texture exit_final = AssetManager::getTexture("images/exit_final.png");
+
+  MusicField mf{"audio/sleep.ogg"};
+  Menu(SceneCompose &cmp, sf::RenderWindow &win) : Scene(cmp), win(win)
+  {
+    button_start.setPosition(750, 200);
+    button_exit.setPosition(790, 400);
+    bg.setTexture(background1);
+    button_start.setTexture(start);
+    button_exit.setTexture(exit);
+  }
+  void draw(sf::RenderTarget &window) override 
+  { 
+    ::draw(window, bg); 
+    ::draw(window, button_start);
+    ::draw(window, button_exit);
+  }
+  bool update(sf::Time dt) override 
+  {
+    //ca_.update(dt);
+    return false;
+  }
+  bool handleEvent(const sf::Event &event) override 
+  { 
+    sf::Vector2i pos = sf::Mouse::getPosition(win);
+    sf::Vector2f mousepos = win.mapPixelToCoords(pos);
+    if(button_start.getGlobalBounds().contains(mousepos.x, mousepos.y))
+    {
+      button_start.setTexture(start_select);
+      if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+      {
+        bg.setTexture(background2);
+        button_start.setTexture(start_final);
+        cmp_.pending_pop();
+        cmp_.pending_push<Mehehenu>(win);
+      }
+    }
+    else if(button_exit.getGlobalBounds().contains(mousepos.x, mousepos.y))
+    {
+      button_exit.setTexture(exit_select);
+      if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+      {
+        button_exit.setTexture(exit_final);
+        cmp_.pending_pop();
+      }
+    }
+    else
+    {
+      button_start.setTexture(start);
+      button_exit.setTexture(exit);
+    }
+    return true; 
+  }
+};
 
 int main() {
-  auto &&aniM = mmed::AnimationManager::getInstance();
-  aniM.loadFile("animations.json");
-  mmed::CharacterAnimation character{{mmed::AnimationManager::getAnimation("na_l"),
-                                mmed::AnimationManager::getAnimation("na_2"),
-                                mmed::AnimationManager::getAnimation("na_3")},
-                               {}};
-  character.select_anim("break_dance");
-
-  auto window = sf::RenderWindow{sf::VideoMode(204, 204), "Test Manager",
+  auto window = sf::RenderWindow{sf::VideoMode(1920, 1080), "Test Manager",
                                  sf::Style::Titlebar | sf::Style::Close};
   sf::Clock clock;
+  SceneCompose scmp = SceneCompose();
+  scmp.pending_push<Menu>(window);
 
   while (window.isOpen()) {
     auto dt = clock.restart();
 
     sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::KeyPressed) {
-        switch (event.key.code) {
-        case sf::Keyboard::B:
-          character.select_anim("break_dance");
-          break;
-        case sf::Keyboard::D:
-          character.select_anim("dance");
-          break;
-        case sf::Keyboard::E:
-          character.select_anim("dissapear");
-          break;
-        default:
-          break;
-        }
-        character.restart();
-      }
+    while (window.pollEvent(event)) 
+    {
+      scmp.handleEvent(event);
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    character.update(dt);
+    scmp.update(dt);
+    if(scmp.empty())
+    {window.close();}
     window.clear();
-    draw(window, character);
+    scmp.draw(window);
     window.display();
   }
   return 0;
