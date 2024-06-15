9e9503543b70874f5f8da04b362543df49f34c95
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-06-15 19:07:24 +0300

    Архитектурно мне не нравится... Но работает, задуманный интерфейс реализован

--- apps/menu_test/main.cc
+++ apps/menu_test/main.cc
@@ -2,50 +2,27 @@
 #include "mmedia/draw.hh"
 #include "scdc/scene_compose.hh"
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
-#include <SFML/Window/Mouse.hpp>
+#include <cstdarg>
 #include <functional>
 #include <iostream>
-
-template <typename T>
-concept mouse_handler = requires(T t) {
-  { t() } -> std::same_as<sf::Vector2i>;
-};
 
 namespace GUI {
 struct Component : sf::Transformable, sf::Drawable {
   using ptr = std::shared_ptr<Component>;
-  virtual bool handleEvent(const sf::Event &) = 0;
-  virtual bool update(sf::Time) = 0;
+  virtual bool handleEvent(const sf::Event &, const sf::Vector2f &pos) = 0;
+  virtual bool update(sf::Time, const sf::Vector2f &pos) = 0;
   virtual void draw(sf::RenderTarget &target,
                     sf::RenderStates states) const = 0;
   virtual ~Component() = default;
 };
 template <typename T>
 concept component_child = std::derived_from<T, Component>;
-struct Container : public Component {
-  std::vector<Component::ptr> cld_;
-  bool update(sf::Time dt) override {
-    return std::any_of(cld_.begin(), cld_.end(),
-                       [dt](auto &&i) { return i->update(dt); });
-  }
-  bool handleEvent(const sf::Event &ev) override {
-    return std::any_of(cld_.begin(), cld_.end(),
-                       [&ev](auto &&i) { return i->handleEvent(ev); });
-  }
-  void draw(sf::RenderTarget &rt, sf::RenderStates states) const override {
-    states.transform *= getTransform();
-    for (auto &&i : cld_)
-      ::draw(rt, *i, states);
-  }
-};
-
 class Button : public Component {
   enum e_states { D, P, R, H };
   std::map<e_states, std::string> state_string = {
@@ -71,33 +48,32 @@ class Button : public Component {
   changed cur_state;
 
 public:
-  std::function<sf::Vector2i()> mh_;
-  std::function<void()> rls_;
+  std::function<void()> prs_, rls_;
   mmed::CharacterAnimation anim_;
   sf::RectangleShape rect_;
-  Button(std::function<sf::Vector2i()> mh, std::function<void()> rls,
+  Button(std::function<void()> prs, std::function<void()> rls,
          const mmed::CharacterAnimation &anim, const sf::RectangleShape &rect)
-      : mh_(mh), rls_(rls), anim_(anim), rect_(rect) {
+      : prs_(prs), rls_(rls), anim_(anim), rect_(rect) {
     anim_.select_anim("default");
     anim_.restart();
   }
-  inline bool contains() {
-    auto [px, py] = mh_();
+  inline bool contains(const sf::Vector2f &p) {
+    auto [px, py] = p;
     auto transform = getInverseTransform();
     auto [x, y] = transform.transformPoint(px, py);
     return rect_.getGlobalBounds().contains(x, y);
   }
-  bool update(sf::Time dt) override {
+  bool update(sf::Time dt, const sf::Vector2f &pos) override {
     anim_.update(dt);
     bool b = cur_state;
 
     switch (cur_state.get()) {
     case D:
-      if (contains())
+      if (contains(pos))
         cur_state = H;
       break;
     case H:
-      if (!contains())
+      if (!contains(pos))
         cur_state = D;
       break;
     case R:
@@ -113,14 +89,14 @@ public:
     }
     return true;
   }
-  bool handleEvent(const sf::Event &ev) override {
+  bool handleEvent(const sf::Event &ev, const sf::Vector2f &pos) override {
     bool b = cur_state;
     if (ev.type == sf::Event::MouseButtonPressed)
       switch (cur_state.get()) {
       case D:
       case H:
-        if (contains())
-          cur_state = P;
+        if (contains(pos))
+          prs_(), cur_state = P;
         break;
       default:
         break;
@@ -143,10 +119,86 @@ public:
     ::draw(target, anim_, states);
   }
 };
-
 } // namespace GUI
-//
+
+struct FollowAnim {
+  mmed::CharacterAnimation anim;
+  void update(sf::Time dt, const sf::Vector2f &pos) {
+    anim.sp_.setPosition(pos);
+    anim.update(dt);
+  }
+  void draw(sf::RenderTarget &rt, sf::RenderStates states) const {
+    ::draw(rt, anim, states);
+  }
+};
+
+template <typename T> struct BoolDrawable {
+  T t;
+  bool check = true;
+  void update(sf::Time dt, const sf::Vector2f &pos) {
+    if (check)
+      t.update(dt, pos);
+  }
+  void draw(sf::RenderTarget &rt, sf::RenderStates states) const {
+    if (check)
+      ::draw(rt, t, states);
+  }
+  void handleEvent(const sf::Event &event, const sf::Vector2f &pos) {
+    if (check)
+      t.handleEvent(event, pos);
+  }
+};
+
+class DD : public GUI::Component {
+  GUI::Button btn_;
+  BoolDrawable<FollowAnim> flw_anim;
+
+public:
+  DD(
+      const mmed::CharacterAnimation &btn_anim, const sf::RectangleShape &rs,
+      const mmed::CharacterAnimation &anim, std::function<void()> prs = [] {},
+      std::function<void()> rls = [] {})
+      : btn_(
+            [this, prs] {
+              prs();
+              flw_anim.check = true;
+            },
+            [this, rls] {
+              rls();
+              flw_anim.check = false;
+            },
+            btn_anim, rs),
+        flw_anim({anim}, false) {}
+  sf::Vector2f inner_pos(const sf::Vector2f &p) {
+    flw_anim.t.anim.select_anim("follow");
+    flw_anim.t.anim.restart();
+    auto [px, py] = p;
+    auto transform = getInverseTransform();
+    return transform.transformPoint(px, py);
+  }
+  bool update(sf::Time dt, const sf::Vector2f &pos) override {
+    btn_.update(dt, inner_pos(pos));
+    return true;
+  }
+  bool follow_update(sf::Time dt, const sf::Vector2f &pos) {
+    auto [x, y] = pos;
+    flw_anim.update(dt, pos);
+    return true;
+  }
+  bool handleEvent(const sf::Event &ev, const sf::Vector2f &pos) override {
+    btn_.handleEvent(ev, pos);
+    return true;
+  }
+  void draw(sf::RenderTarget &target, sf::RenderStates states) const override {
+    ::draw(target, btn_, states);
+  }
+  void follow_draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    ::draw(target, flw_anim, states);
+  }
+};
+
 struct MenuScene : scdc::Scene {
+  sf::View view;
   std::vector<mmed::Animation> button_anim = {
       mmed::AnimationManager::getAnimation("b1_0"),
       mmed::AnimationManager::getAnimation("b1_1"),
@@ -154,30 +206,60 @@ struct MenuScene : scdc::Scene {
       mmed::AnimationManager::getAnimation("b1_d"),
   };
   sf::RenderWindow &win_;
-
-  std::function<sf::Vector2i()> get_pos() {
-    return [this]() { return sf::Mouse::getPosition(win_); };
-  }
+  struct tmp_view {
+    sf::RenderWindow &win_;
+    tmp_view(sf::RenderWindow &win, const sf::View &view) : win_(win) {
+      win.setView(view);
+    }
+    ~tmp_view() { win_.setView(win_.getDefaultView()); }
+  };
 
   GUI::Button btn;
+  DD dd;
   MenuScene(scdc::SceneCompose &sc, sf::RenderWindow &win)
       : scdc::Scene(sc), win_(win),
-        btn{get_pos(),
-            [] { std::cout << "Foo" << std::endl; },
+        btn{[] { std::cout << "Foo" << std::endl; },
+            [] { std::cout << "Bar" << std::endl; },
             mmed::CharacterAnimation(button_anim, {}),
-            sf::RectangleShape{{100, 50}}} {
+            sf::RectangleShape{{100, 50}}},
+        dd(
+            mmed::CharacterAnimation{button_anim, {}},
+            sf::RectangleShape{{100, 50}},
+            mmed::CharacterAnimation{
+                {mmed::Animation::one_frame_anim("follow", "images/start.png")},
+                {}},
+            [] { std::cout << "Foo\n"; }, [] { std::cout << "Bar\n"; }) {
+    view.setViewport({.25, .25, .5, .5});
     btn.move(120, 100);
     btn.rotate(-30);
     btn.scale({2, 2});
   }
 
-  void draw() override { ::draw(win_, btn); }
+  void draw() override {
+    {
+      tmp_view tv(win_, view);
+      ::draw(win_, btn);
+      ::draw(win_, dd);
+    }
+    dd.follow_draw(win_, sf::RenderStates::Default);
+  }
+  sf::Vector2f world_pos() {
+    sf::Vector2i pixelPos = sf::Mouse::getPosition(win_);
+    return win_.mapPixelToCoords(pixelPos);
+  }
   bool update(sf::Time dt) override {
-    btn.update(dt);
+    {
+      tmp_view tv(win_, view);
+      btn.update(dt, world_pos());
+      dd.update(dt, world_pos());
+    }
+    dd.follow_update(dt, world_pos());
     return true;
   }
   bool handleEvent(const sf::Event &event) override {
-    btn.handleEvent(event);
+    tmp_view tv(win_, view);
+    btn.handleEvent(event, world_pos());
+    dd.handleEvent(event, world_pos());
     return true;
   }
 };
