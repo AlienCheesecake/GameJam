d58cea1b8053632452e3677108428cc5db40fad6
Naumikhin Daniil <dan.naumihin@yandex.ru>
2024-06-17 16:50:33 +0300

    Стек очередей треков

--- apps/menu_test/main.cc
+++ apps/menu_test/main.cc
@@ -5,6 +5,7 @@
 #include "mmedia/MusicPlayer.hh"
 #include "mmedia/draw.hh"
 #include "scdc/scene_compose.hh"
+#include <SFML/Audio/Music.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
@@ -13,15 +14,19 @@
 #include <SFML/Graphics/Transformable.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <algorithm>
+#include <concepts>
 #include <functional>
+#include <initializer_list>
 #include <iostream>
-#include <mutex>
+#include <iterator>
+#include <queue>
+#include <stack>
 #include <vector>
-#include <thread>
 
 using namespace mmed::gmui;
 using namespace mmed;
 
+#if 0
 struct BeginLoopMusicField {
   bool was_paused;
   std::string loop_path_, begin_path_, prev_path_;
@@ -66,6 +71,139 @@ struct BeginLoopMusicField {
     mp.play(prev_path_);
   }
 };
+#endif
+struct MusicDef {
+  std::string path;
+  bool is_loop;
+};
+
+struct MusicQueue {
+  template <typename Iter>
+    requires std::same_as<MusicDef,
+                          typename std::iterator_traits<Iter>::value_type>
+  MusicQueue(Iter fst, Iter lst) {
+    for (; fst != lst; ++fst)
+      queue.push(*fst);
+    play_front();
+  }
+  MusicQueue(std::initializer_list<MusicDef> il) : queue(il) { play_front(); }
+  void update() {
+    if (plr.getStatus() == sf::Music::Stopped && !queue.empty()) {
+      play_front();
+      queue.pop();
+    }
+  }
+
+  void play_front() {
+    auto &&tmp = queue.front();
+    plr.play(tmp.path);
+    plr.setLoop(tmp.is_loop);
+  }
+
+private:
+  std::queue<MusicDef> queue;
+  mmed::MusicPlayer &plr = mmed::MusicPlayer::getInstance();
+};
+
+struct MusicStackOfQueues {
+  void update() {
+    if (!stack.empty())
+      stack.top().update();
+  }
+  void push(MusicQueue &&q) {
+    stack.push(q);
+    auto &&plr = MusicPlayer::getInstance();
+    plr.stop();
+  }
+  void push(const MusicQueue &q) {
+    stack.push(q);
+    auto &&plr = MusicPlayer::getInstance();
+    plr.stop();
+  }
+  template <typename... Args> void emplace(Args &&...args) {
+    stack.emplace(std::forward<Args>(args)...);
+    auto &&plr = MusicPlayer::getInstance();
+    plr.stop();
+  }
+  void pop() {
+    // if (!stack.empty())
+      stack.pop();
+    if (!stack.empty())
+      stack.top().play_front();
+  }
+
+private:
+  std::stack<MusicQueue> stack;
+};
+
+struct NMusicField {
+  MusicStackOfQueues &sck;
+  NMusicField(MusicStackOfQueues &stack, std::initializer_list<MusicDef> il)
+      : sck(stack) {
+    stack.emplace(il);
+  }
+  ~NMusicField() { sck.pop(); }
+};
+
+#if 0
+struct MusicStack {
+  void push(std::string_view path, bool is_loop) {
+    stack.emplace(path, is_loop);
+  }
+
+  void push(const MusicDef &msc) { stack.push(msc); }
+  void push(MusicDef &&msc) { stack.push(msc); }
+
+  void pop() {
+    auto &&tmp = stack.top();
+    plr.play(tmp.path);
+    plr.setLoop(tmp.is_loop);
+
+    stack.pop();
+  }
+  
+  void update() {
+    if (plr.getStatus() != sf::Music::Status::Playing)
+      pop();
+  }
+
+private:
+  std::stack<MusicDef> stack;
+  MusicPlayer &plr = MusicPlayer::getInstance();
+};
+
+struct MusicStackField {
+  template <typename Iter>
+    requires std::same_as<MusicStack::MusicDef,
+                          typename std::iterator_traits<Iter>::value_type>
+  MusicStackField(MusicStack &stack, Iter fst, Iter lst) : stk(stack) {
+    size_t cnt = 0;
+    for (; fst != lst; ++fst, ++cnt)
+      stack.push(*fst);
+    stack.pop();
+    tracks_enqueued = --cnt;
+  }
+
+  MusicStackField(MusicStack &stack,
+                  std::initializer_list<MusicStack::MusicDef> il)
+      : stk(stack) {
+    size_t cnt = 0;
+    for (auto &&i : il)
+      stack.push(i);
+    stack.pop();
+    tracks_enqueued = --cnt;
+  }
+
+  ~MusicStackField() {
+    for (; tracks_enqueued != 0; --tracks_enqueued)
+      stk.pop();
+  }
+
+private:
+  size_t tracks_enqueued;
+  MusicStack &stk;
+};
+#endif
 
 namespace mmed::gmui {
 struct Container : Component {
@@ -101,10 +239,16 @@ struct Container : Component {
 
 struct Menu2 : scdc::Scene {
   // MusicPauseField mpf;
+  MusicStackOfQueues &ms_;
+  NMusicField mf_{ms_,
+                  {{"audio/piano_main_menu_begin.ogg", false},
+                   {"audio/piano_main_menu_loop.ogg", true}}};
+#if 0
   BeginLoopMusicField mf {
     "audio/piano_main_menu_begin.ogg",
     "audio/piano_main_menu_loop.ogg"
   };
+#endif
   sf::RenderWindow &win_;
 
   mmed::CharacterAnimation button_anim = {
@@ -128,8 +272,9 @@ struct Menu2 : scdc::Scene {
   Container menu = {
       {Component::ptr{
            new DD(button_anim, sf::RectangleShape({100, 50}), follow, "start")},
-       Component::ptr{
-           new DD(button_anim, sf::RectangleShape({100, 50}), follow, "exit")},
+       Component::ptr{new DD(
+           button_anim, sf::RectangleShape({100, 50}), follow, "exit", [] {},
+           [this] { cmp_.pending_pop(); })},
        Component::ptr(new Button([this] { move = true; },
                                  [this] {
                                    move = false;
@@ -138,8 +283,8 @@ struct Menu2 : scdc::Scene {
                                  button_anim, sf::RectangleShape{{100, 50}}))}};
 #endif
 
-  Menu2(scdc::SceneCompose &sc, sf::RenderWindow &win)
-      : scdc::Scene(sc), win_(win) {
+  Menu2(scdc::SceneCompose &sc, sf::RenderWindow &win, MusicStackOfQueues &ms)
+      : scdc::Scene(sc), win_(win), ms_(ms) {
 #if 1
     follow.t.sp_.setScale(.4, .4);
     for (size_t i = 0; i < 3; ++i) {
@@ -170,8 +315,10 @@ struct Menu2 : scdc::Scene {
 };
 
 struct MenuScene : scdc::Scene {
+#if 0
   BeginLoopMusicField mf{"audio/main_menu_begin.ogg",
                          "audio/main_menu_loop.ogg"};
+#endif
   sf::View view;
   std::vector<mmed::Animation> button_anim = {
       mmed::AnimationManager::getAnimation("b1_0"),
@@ -187,15 +334,22 @@ struct MenuScene : scdc::Scene {
       false};
 
   sf::RenderWindow &win_;
+  MusicStackOfQueues &ms_;
+  NMusicField mf{ms_,
+                 {
+                     {"audio/main_menu_begin.ogg", false},
+                     {"audio/main_menu_loop.ogg", true},
+                 }};
 
   Button btn;
   DD dd;
-  MenuScene(scdc::SceneCompose &sc, sf::RenderWindow &win)
-      : scdc::Scene(sc), win_(win),
+  MenuScene(scdc::SceneCompose &sc, sf::RenderWindow &win,
+            MusicStackOfQueues &ms)
+      : scdc::Scene(sc), win_(win), ms_(ms),
         btn{[] { std::cout << "Foo" << std::endl; },
             [this] {
-              cmp_.pending_pop();
-              cmp_.pending_push<Menu2>(win_);
+              // cmp_.pending_pop();
+              cmp_.pending_push<Menu2>(win_, ms_);
             },
             mmed::CharacterAnimation(button_anim, {}),
             sf::RectangleShape{{100, 50}}},
@@ -241,12 +395,15 @@ struct MenuScene : scdc::Scene {
 };
 
 int main() {
+  MusicStackOfQueues ms;
+
   scdc::SceneCompose sc;
   auto &&aniM = mmed::AnimationManager::getInstance();
   aniM.loadFile("animations.json");
   auto window = sf::RenderWindow{sf::VideoMode(500, 300), "Test Manager",
                                  sf::Style::Titlebar | sf::Style::Close};
-  sc.pending_push<MenuScene>(window);
+  // MusicStack ms;
+  sc.pending_push<MenuScene>(window, ms);
   sf::Clock clck;
   while (window.isOpen()) {
     auto dt = clck.restart();
@@ -257,6 +414,7 @@ int main() {
         window.close();
     }
     sc.update(dt);
+    ms.update();
     if (sc.empty())
       window.close();
     window.clear();
